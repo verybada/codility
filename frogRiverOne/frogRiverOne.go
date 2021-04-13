561119c7efc1ee1508386526f76e20db34b92210
verybada <verybada.lin@gmail.com>
2021-04-13 22:45:44 +0800

    add frogRiverOne

---
+++ frogRiverOne/frogRiverOne.go
@@ -0,0 +1,16 @@
+package frogRiverOne
+
+func Solution(X int, A []int) int {
+	leaves := make(map[int]bool)
+	for second, pos := range A {
+		if pos > X {
+			continue
+		}
+		leaves[pos] = true
+		if len(leaves) == X {
+			return second
+		}
+	}
+
+	return -1
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/verybada/codility
+
+go 1.16
